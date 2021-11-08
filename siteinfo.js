9fe2aac0daf5d81d53f15516fd6e7fc5111bd133
netren <fslyfeng@gmail.com>
2021-11-08 19:16:38 +0800

    first add

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "资源邦源码网",
+    uniacid: "3",
+    acid: "3",
+    multiid: "0",
+    version: "3.1.53",
+    siteroot: "https://www.wazyb.com/app/index.php",
+    design_method: "3"
+};
